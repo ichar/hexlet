a46a3a8689de6ee5c0e498e1556c9a041440860c
ichar <ichar.xx@gmail.com>
2022-05-27 16:51:44 +0300

    enlarge

---
+++ enlarge.py
@@ -0,0 +1,25 @@
+def show(image):
+    for line in image:
+        print(line)
+
+def enlarge(arr):
+    out = []
+    for row in arr:
+        line= ''
+        for x in row:
+            line+= x*(2)
+            
+        out.append(line)
+        out.append(line)
+
+    return out
+
+image = [
+    '***',
+    '* *',
+    '* *',
+    '***',
+]
+
+show(image)
+show(enlarge(image))
