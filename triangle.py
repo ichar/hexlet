f25257c0c64a6f272808f27ab3fe17a48b0c500f
ichar <ichar.xx@gmail.com>
2022-05-27 14:36:37 +0300

    triangle

--- triangle.py
+++ triangle.py
@@ -3,29 +3,29 @@ import sys
 IsTrace=0
 
 def triangle(n):
-  line = []
-  if n > 4:
-    p=triangle(n-1)
-    for index in range(0, n+1):
-      if index in (0, n):
-        line.append(1)
-        s=1
-      else:
-        line.append(s+p[index])
-        s=p[index]
-      if IsTrace:
-          print('>>>index:', index, s)
-      
-  else:
-    for index in range(1, n+2):
-        if index == 1 or index == n+1:
-            line.append(1)
-        elif index < n+1:
-            if index%2 == 0 or (n-index)%2==0:
-                line.append(n)
-            elif index%2 == 1:
-                line.append(sum(line[0:index-1])+1)
-  return line
+    line = []
+    if n > 4:
+        p = triangle(n-1)
+        for index in range(0, n+1):
+            if index in (0, n):
+                line.append(1)
+                s = 1
+            else:
+                _ = p[index]
+                line.append(s+_)
+                s = _
+            if IsTrace:
+                print('>>>index:', index, s)
+    else:
+        for index in range(1, n+2):
+            if index == 1 or index == n+1:
+                line.append(1)
+            elif index < n+1:
+                if index % 2 == 0 or (n-index) % 2 == 0:
+                    line.append(n)
+                elif index % 2 == 1:
+                    line.append(sum(line[0:index-1])+1)
+    return line
 
 
 if __name__ == "__main__":
